00f0134ef3c0c464a87d1004f052398048add8ec
anthonykao10 <anthonykao10@gmail.com>
2019-09-20 01:01:24 +0000

    Update "addProperty" to query db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -162,9 +162,46 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `
+    INSERT INTO properties (
+      title, 
+      description, 
+      owner_id, 
+      cover_photo_url, 
+      thumbnail_photo_url, 
+      cost_per_night, 
+      parking_spaces, 
+      number_of_bathrooms, 
+      number_of_bedrooms, 
+      province, 
+      city, 
+      country, 
+      street, 
+      post_code,
+      active
+    )
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, true)
+    RETURNING *;
+  `;
+  const queryParams = [
+    property.title,
+    property.description, 
+    property.owner_id, 
+    property.cover_photo_url, 
+    property.thumbnail_photo_url, 
+    property.cost_per_night, 
+    property.parking_spaces, 
+    property.number_of_bathrooms, 
+    property.number_of_bedrooms, 
+    property.province, 
+    property.city, 
+    property.country, 
+    property.street, 
+    property.post_code
+  ];
+
+  return pool.query(queryString, queryParams)
+    .then(res => res.rows[0])
+    .catch(err => console.log('\nerror creating property:\n', err));
 }
 exports.addProperty = addProperty;
